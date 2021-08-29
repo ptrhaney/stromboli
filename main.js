1b5bf55346e152d7737a507b72df8e09fcb53cd5
ptrhaney <84342049+ptrhaney@users.noreply.github.com>
2021-08-29 16:08:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -133,8 +133,8 @@ require([
           field: "mag",
           axis: "all",
           stops: [
-            { value: 2.5, size: 30000, label: "<15%" },
-            { value: 7, size: 50000, label: "25%" }
+            { value: 2.5, size: 10000, label: "<15%" },
+            { value: 7, size: 20000, label: "25%" }
           ]
         },
         {
